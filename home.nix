59c7f0256d4473058cc5aa2537c25627d7c2c6cc
free <mpdf2023@outlook.com>
2026-09-12 17:20:03 +0800

    feat(ssh): enable ssh-agent

--- home.nix
+++ home.nix
@@ -25,6 +25,20 @@
     };
   };
 
+  programs.ssh = {
+    enable = true;
+   
+    addKeysToAgent = "yes";
+ 
+    matchBlocks = {
+      github = {
+        hostname = "github.com";
+        user = "git";
+        identityFile = "~/.ssh/id_25519_github";
+      };
+    };
+  };
+
   programs.zsh = {
     enable = true;
     enableCompletion = true;
