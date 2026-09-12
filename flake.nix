57d5e840996d25e567de8b3619eccbc878db31bf
free <mpdf2023@outlook.com>
2026-09-12 21:26:56 +0800

    refactor(hosts): use default.nix as VM entry point

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
 
         # 这里导入之前我们使用的 configuration.nix，
         # 这样旧的配置文件仍然能生效
-        ./hosts/vm/configuration.nix
+        ./hosts/vm
         
         home-manager.nixosModules.home-manager 
 
