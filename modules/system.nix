46c74394ac81a2b9a63b52f3a8e86c5f4f9a6de6
free <mpdf2023@outlook.com>
2026-09-14 17:54:31 +0800

    feat(zsh): add p10k theme

--- modules/system.nix
+++ modules/system.nix
@@ -1,7 +1,7 @@
 { ... }:
 {
   imports = [
-    ./system/boot.nix ./system/desktop.nix ./system/locale.nix
+    ./system/boot.nix ./system/desktop.nix ./system/locale.nix ./system/fonts.nix
     ./system/networking.nix ./system/nix.nix ./system/packages.nix
     ./system/programs.nix ./system/services.nix
     ./system/state.nix ./system/users.nix
--- modules/system/fonts.nix
+++ modules/system/fonts.nix
@@ -1,4 +1,4 @@
-{ pkg, ... }:
+{ pkgs, ... }:
 
 {
   fonts.packages = with pkgs; [
